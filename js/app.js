c44120ca02cf3e9bcf0775b4d3b3809ca1827bbb
Teresa Nededog <tnededog@my.hpu.edu>
2015-03-09 11:20:52 -1000

    Deploying MEAN stack

--- js/app.js
+++ js/app.js
@@ -6,11 +6,26 @@ angular
   .controller('TodoController', ['$scope', function($scope){
     
     // $scope.name = "IS HOOKED UP" NOW WE KNOW CONTROLLER IS HOOKED UP;
-    $scope.todos = [
-      {
-        title: "Fake title",
+    // $scope.todos = [
+    //   {
+    //     title: "Fake title",
+    //     completed: true
+    //   }
+    $scope.todos = [];
+
+    $scope.save_todo = function (new_title) {
+      $scope.todos.push({
+        title: new_title,
         completed: false
+      });
+    
+      $scope.new_todo = ""; //clear the input
+    };
+    
+    $scope.enter_saves = function($event){
+      if( $event.keyCode == 13){ //keyCode for [enter key]
+        $scope.save_todo( $scope.new_todo);
       }
-    ];
+    };
 
   }]);
